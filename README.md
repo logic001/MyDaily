d09e7746c79d85e55fe1c678740a4f2e634239b8
George <logic955@foxmail.com>
2017-06-17 13:25:37 +0800

    README
    version 2
    右上角添加了查询特定日期消费记录的功能按钮

---
+++ README.md
@@ -0,0 +1,6 @@
+# MyDaily
+version 1 .This is an app for recording Daily cost.
+
+1.使用Bmob云服务数据库进行 *每条消费记录的保存* 和 *总消费记录查询功能*
+
+2.使用的是ListView进行数据展示
--- app/src/main/java/com/ironmen/mydaily/MainActivity.java
+++ app/src/main/java/com/ironmen/mydaily/MainActivity.java
@@ -24,11 +24,14 @@ import cn.bmob.v3.exception.BmobException;
 import cn.bmob.v3.listener.FindListener;
 import cn.bmob.v3.listener.SaveListener;
 
+
 public class MainActivity extends AppCompatActivity {
+
 
     private List<CostBean> mCostBeanList = new ArrayList<>();
     CostBean costBean;
     private CostListAdapter adapter;
+    private String searchDate = "";
 
 
     @Override
@@ -39,11 +42,11 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        Bmob.initialize(this,"8328498fdda0c8dc6c372a5fc00d110c");
-        costBean = new CostBean();
+        Bmob.initialize(this, "8328498fdda0c8dc6c372a5fc00d110c");  //Bmob云服务器初始化
+        costBean = new CostBean();   //新建全局bean,为添加数据作准备
 
         final ListView costList = (ListView) findViewById(R.id.lv_mydaily);
-        initCostData(); //进入程序时初始化数据************
+        initCostData(); //进入程序时初始化数据
 
         adapter = new CostListAdapter(mCostBeanList, this);
         costList.setAdapter(adapter);
@@ -64,7 +67,7 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         costBean.setCostTitle(title.getText().toString());
-                        costBean.setCostMoney( money.getText().toString());
+                        costBean.setCostMoney(money.getText().toString());
                         costBean.setCostDate(date.getYear() + "-" + (date.getMonth() + 1) + "-" + date.getDayOfMonth());
 
                         costBean.save(new SaveListener<String>() {
@@ -149,12 +152,65 @@ public class MainActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+        switch (id) {
+            case R.id.action_settings:
+                Toast.makeText(this, "click settings", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_search:
+                newSearchView();   //创建一个查询窗口,并完成查询更新操作
+                return true;
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
         }
 
+        //noinspection SimplifiableIfStatement
+
+
         return super.onOptionsItemSelected(item);
+    }
+
+    private void newSearchView() {   //点击查询按钮，新建查询会话窗口
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Search Record");
+        LayoutInflater inflater = LayoutInflater.from(MainActivity.this);
+        final View searchView = inflater.inflate(R.layout.date_search, null);
+        builder.setView(searchView);
+        final DatePicker dp_search_data = (DatePicker) searchView.findViewById(R.id.dp_search_date);
+
+        builder.setPositiveButton("Search!", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                searchDate = dp_search_data.getYear() + "-" + (dp_search_data.getMonth() + 1) + "-" + dp_search_data.getDayOfMonth();
+                goSearch();
+            }
+        });
+        builder.create().show();
+
+    }
+
+    private void goSearch() {   //查询核心代码
+
+        BmobQuery<CostBean> dateSearchCostBean = new BmobQuery<>();
+        dateSearchCostBean.addWhereEqualTo("costDate", searchDate);
+        dateSearchCostBean.findObjects(new FindListener<CostBean>() {
+            @Override
+            public void done(List<CostBean> list, BmobException e) {
+                if(e == null) {
+                    mCostBeanList.removeAll(mCostBeanList);
+                    adapter.notifyDataSetChanged();
+                    mCostBeanList.addAll(list);
+                    adapter.notifyDataSetChanged();
+                    Toast.makeText(MainActivity.this,"success",Toast.LENGTH_SHORT).show();
+                } else{
+                    Toast.makeText(MainActivity.this,"fail",Toast.LENGTH_SHORT).show();
+                }
+
+                /*AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setMessage(list.toString());
+                builder.create().show();*/
+
+
+            }
+        });
+
     }
 }
